d443d3743e565a197d2d9b3219141d02ae8655c1
Suhoy95 <suhoy95@gmail.com>
2016-12-11 00:51:10 +0500

    'done

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.eee').slick({
+        dots: true,
+        infinite: true,
+        speed: 750,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        centerMode: true,
+        slidesToShow: 3
+    });
+});
